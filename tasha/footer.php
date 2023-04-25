ada87dcf1b9dd64e163ec210736d98e5d4dacacd
Hoàng Quốc Cường <cuong.hq.2235@aptechlearning.edu.vn>
2023-04-25 22:07:22 +0700

    update link to header and create categoryTypePage

--- tasha/footer.php
+++ tasha/footer.php
@@ -1,4 +1,3 @@
-<!-- Phần footer của trang dùng để import vào các trang home, category, product ... -->
 <footer>
   <div class="container">
     <div class="row">
@@ -11,7 +10,6 @@
       </div>
       <div class="col-8">
         <div class="row">
-          <!-- Tab bootstrap Dùng để tạo tab khi click vào địa chỉ -->
           <div class="col-3">
             <div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
               <a class="nav-link active" id="trieu-khuc-tab" data-toggle="pill" href="#trieu-khuc" role="tab" aria-controls="v-pills-home" aria-selected="true">Triều Khúc</a>
@@ -22,7 +20,6 @@
           </div>
           <div class="col-9">
             <div class="tab-content">
-              <!-- Các iframe địa chỉ, phần này có thể lên google copy -->
               <div class="tab-pane fade show active" id="trieu-khuc" role="tabpanel" aria-labelledby="trieu-khuc-tab">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7450.541753670525!2d105.79562539173128!3d20.981776240109713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acc3ae37fc47%3A0x607ded9d4d1d1182!2zVHJp4buBdSBLaMO6YywgVMOibiBUcmnhu4F1LCBUaGFuaCBUcsOsLCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1681918682315!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
@@ -42,18 +39,13 @@
     </div>
   </div>
 </footer>
-<!-- Phần này sẽ là phần viết các function javascript -->
-<!-- Nhớ viết bên trong thẻ đóng mở <script>Viết ở đây</script> -->
 <script>
-  // Phần này để cài đặt thời gian tự động next slide của banner=> có thể tìm hiều trong phần slide bootstrap
   const banner = document.getElementById('bannerHome')
   banner.carousel({
     interval: 100
   })
 </script>
 
-<!-- Phần này để import file js -->
-<!-- các link dưới đang dùng link cdn để thay cho các file js -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- tasha/header.php
+++ tasha/header.php
@@ -1,4 +1,3 @@
-<!-- Header component => Tạo header chung cho tấT cả các trang -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,43 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasha</title>
-    <!-- Phần này để import file css -->
-<!-- các link dưới đang dùng link cdn để thay cho các file css -->
 
-<!-- link dưới là import boostrap.css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- link dưới là import frontawesome để dùng icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- link dưới là import style.css -->
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
     <header>
-        <!-- Phần top header trên đầu -->
         <div style="background: #1E1E1E">
             <div class="d-flex justify-content-end container ">
-                <!-- Phần này để link tới trang đăng ký và đăng nhập -->
                 <a class="pt-1 pb-1 pr-2 pl-2 text-white" href="">Sign up</a>
                 <a class="pt-1 pb-1 pr-2 pl-2 text-white" href="">Sign in</a>
             </div>
         </div>
-        <!-- Phần header ở giữa, form search -->
         <div class="container pt-2 pb-2 d-flex justify-content-between align-items-center">
-            <img src="./assets/images/logo.png" alt="">
-            <!-- Phần này để search sản phẩm -->
+            <a href="./homePage.php"><img src="./assets/images/logo.png" alt=""></a>
             <form action="" class="form-search">
                 <input type="text" placeholder="What are you looking for?">
                 <button type="submit" class="btn  btn-container ">Search</button>
             </form>
             <div>
-                <!-- Phần này là mở giỏ hàng và hiện thị số lượng hàng trong giỏ -->
                 <i style="font-size:25px" class="fa-solid fa-cart-shopping"></i>
                 <span>Cart:2</span>
             </div>
         </div>
         <hr>
-        <!-- Phần menu danh mục dùng navbar của bootstrap -->
         <nav class="navbar navbar-expand-lg container p-0">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -52,14 +40,19 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
-                        <!-- Menu danh mục -->
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Cookware
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <!-- Các loại danh mục -->
-                            <a class="dropdown-item" href="#">Cooking Pans</a>
-                            <a class="dropdown-item" href="#">Frying Pans</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=1">Cooking Pans</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=2">Frying Pans</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=3">Cookware Sets</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=4">Handis</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=5">Kadais [Karahi]</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=6">Grill pans</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=7">Tawas</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=8">Egg Poachers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_type_id=9">Steamers</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -67,8 +60,8 @@
                             Refrigeration
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Freezers</a>
-                            <a class="dropdown-item" href="#">Refrigerators</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=10">Freezers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=11">Refrigerators</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -76,8 +69,12 @@
                             Appliances
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Bread Makers</a>
-                            <a class="dropdown-item" href="#">Coffee Makers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=12">Bread Makers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=13">Coffee Makers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=14">Ice-Cream and sorbet makers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=15">Electric Kettles</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=16">Food Choppers</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=17">Toasters</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -85,8 +82,11 @@
                             Food Storage
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Spice Jars</a>
-                            <a class="dropdown-item" href="#">Storage Bags</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=18">Flask</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=19">Spice Jars</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=20">Storage Bags</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=21">Lunch Boxes</a>
+                            <a class="dropdown-item" href="categoryPage.php?category_id=22">Vacuum bottles</a>
                         </div>
                     </li>
                 </ul>
