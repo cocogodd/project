3b75d509d9ee39d6e13b8c5214027fac7a8497af
huyen1703 <huyen.ntt.2230@aptechlearning.edu.vn>
2023-05-16 22:57:35 +0700

    product detail

--- tasha/productDetail.php
+++ tasha/productDetail.php
@@ -45,18 +45,6 @@ $bestSeller = $conn->query("SELECT product.*,categories.menu_name,categories.id 
         LIMIT $best_seller_start, $limit
         ");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>product_details</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://fontawesome.com/icons/truck-fast?f=sharp&s=regular">
-    <link rel="stylesheet" href="./assets/css/style.css">
     <style>
     
 
@@ -268,9 +256,7 @@ $bestSeller = $conn->query("SELECT product.*,categories.menu_name,categories.id 
             </ul>
         </nav>
     </div>
-</body>
 
-</html>
 
 
 <?php
