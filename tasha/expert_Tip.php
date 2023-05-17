fffc9f8eb40d89acd923b3cbf1347fa44441ef24
phamtuan25 <tuan.pd.2233@apteachlearning>
2023-05-17 22:12:46 +0700

    fix expert

--- tasha/expert_Tip.php
+++ tasha/expert_Tip.php
@@ -1,90 +1,98 @@
+
 <?php
 include('header.php');
 ?>
+  <style>
+    h1{
+      text-align:center;
+      color:#FF6600	;
+      font-family: "TahomaGeneva", sans-serif;
+      size:20px;
+      padding: 30px;
+    }
+    
+    .box{
+      padding-bottom:30px;
+    }
+    .cart{
+      border: 1px solid black;
+      border-radius: 21px; 
+      background-image: linear-gradient(white, #ea6222);
+      padding : 30px;
+      width: 50%;
+      height: 66%;
+      margin-left:auto;
+      margin-right:auto;
+    }
 
+    .title{
+      font-family: "TahomaGeneva", sans-serif;
+      text-shadow: 0px 2px 8px rgba(128,128,128,1);
+    }
 
-<!DOCTYPE html>
-<html style="font-size: 16px;" lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="keywords" content="​Top tips from the experts, INTUITIVE">
-    <meta name="description" content="">
-    <title>Home</title>
-    <link rel="stylesheet" href="//capp.nicepage.com/1f407d6fb52f4835c71b9ab9ebace889f288567a/nicepage.css" media="screen">
-    <link rel="stylesheet" href="./assets/css/nicepage-site.css" media="screen">
-    <link rel="stylesheet" href="./assets/css/Home.css" media="screen">
-    <script class="u-script" type="text/javascript" src="//capp.nicepage.com/assets/jquery-3.5.1.min.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="//capp.nicepage.com/1f407d6fb52f4835c71b9ab9ebace889f288567a/nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 5.9.10, nicepage.com">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
-
-</head>
-  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-d6a8"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-      
-        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
-          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
-            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
-              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
-              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
-</g></svg>
-            </a>
-          </div>
-          
-</header>
-    <section class="u-align-center u-clearfix u-white u-section-1" id="carousel_9d9a">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        
-        <h2 class="u-custom-font u-font-montserrat u-text u-text-custom-color-1 u-text-default u-text-2"> Top tips from the experts</h2>
-        <div class="u-list u-list-1">
-          <div class="u-repeater u-repeater-1">
-            <div class="u-align-left u-border-2 u-border-grey-75 u-container-style u-image u-list-item u-repeater-item u-shading u-image-1" data-image-width="1200" data-image-height="676">
-              <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-1"><span class="u-custom-color-1 u-file-icon u-icon u-icon-rounded u-radius-20 u-icon-1"><img src="https://assets.nicepagecdn.com/9de13a7a/4924205/images/4748274-996611db.png" alt=""></span>
-                <div class="u-align-left u-container-style u-grey-10 u-group u-radius-20 u-shape-round u-group-1">
-                  <div class="u-container-layout u-container-layout-2">
-                    <h5 class="u-custom-font u-font-montserrat u-text u-text-3">Cookware</h5>
-                    <p class="u-text u-text-4"> To choose a good cookware, you should note the material, size, thickness, ease of cleaning and suitable price.</p>
-                  </div>
-                </div>
+    p{
+      font-family: "Times New Roman Times", sans-serif;
+      font-size:20px;
+      text-shadow: 0px 2px 5px white;
+    }
+  </style>
+    <section class="">
+      <div class="">
+        <h1 class=""> Top tips from the experts</h1>
+        <div class="">
+          <div class="">
+            <div class="box">
+              <div class="cart">
+                <h2 class="title">Cookware</h2>
+                <p class=""> Choosing cookware is an important decision to make sure that you can cook deliciously and safely. Here are some expert tips on choosing cookware: <br>
+                  <br>1. Choose high-quality cookware: High-quality cookware will help you cook better and prolong their life. Choose cookware made from high-quality materials such as stainless steel, copper, aluminum or cast iron. <br>
+                  <br>2. Choose the right cookware for your needs: Choose the right cookware for your needs. If you often cook for a large family, you should choose a larger pot. If you often cook for one or two people, you can choose a smaller pot. <br>
+                  <br>3. Choose cookware that is easy to use: Choose cookware that is easy to use and easy to clean. Choose cookware with comfortable and non-slip handles to avoid accidents while cooking. <br>
+                  <br>4. Choose safe cooking utensils: Choose safe cooking utensils that are not harmful to health. Choose cookware that does not contain harmful substances such as PFOA or PFAS. <br>
+                  <br>5. Choose cookware that fits your budget: Choose cookware that fits your budget. High-quality cookware often costs more, so you should think carefully before buying.
+                </p>
               </div>
             </div>
-            <div class="u-align-left u-border-2 u-border-grey-75 u-container-style u-image u-list-item u-repeater-item u-shading u-image-2" data-image-width="338" data-image-height="149">
-              <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-3"><span class="u-custom-color-1 u-file-icon u-icon u-icon-rounded u-radius-20 u-icon-2"><img src="https://assets.nicepagecdn.com/9de13a7a/4924205/images/2969229-31f04a13.png" alt=""></span>
-                <div class="u-align-left u-container-style u-grey-10 u-group u-radius-20 u-shape-round u-group-2">
-                  <div class="u-container-layout u-container-layout-4">
-                    <h5 class="u-custom-font u-font-montserrat u-text u-text-5">Refrigeration</h5>
-                    <p class="u-text u-text-6"> Choose the right refrigerator, place it in a cool place, do not overload, maintain and clean it periodically to save electricity and increase durability.</p>
-                  </div>
-                </div>
+            <div class="box">
+              <div class="cart">
+                <h2 class="title"> Refrigeration</h2>
+                <p class=""> Choosing refrigeration equipment is an important decision to ensure that your product is in fact well managed and safe. Here are some expert tips on choosing refrigeration equipment: <br>
+                  <br>1. Choose high-quality refrigeration equipment: Choosing high-quality refrigeration equipment will help you better manage your products and extend their life. Choose refrigeration equipment made from high-quality and durable materials. <br>
+                  <br>2. Choose the right refrigerating appliance for your needs: Choose the right refrigerating appliance for your needs. If you often buy food in bulk, you should choose a larger refrigerator. If you live alone or with only a few people, you can choose a smaller refrigerator. <br>
+                  <br>3. Choose energy-saving refrigeration equipment: Choosing energy-saving refrigeration equipment will help you save energy and protect the environment. Choose refrigeration equipment with an energy-efficient label or with an energy-saving feature. <br>
+                  <br>4. Choose a refrigerator that is easy to use: Choose a refrigerator that is easy to use and easy to clean. Choose chillers that are designed to be simple and easy to take out and put in. <br>
+                  <br>5. Choose the chiller that fits your budget: Choose the chiller that fits your budget. High-quality refrigeration equipment often costs more, so you should think carefully before buying.
+                </p>
               </div>
             </div>
-            <div class="u-align-left u-border-2 u-border-grey-75 u-container-style u-image u-list-item u-repeater-item u-shading u-image-3" data-image-width="750" data-image-height="500">
-              <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-5"><span class="u-custom-color-1 u-file-icon u-icon u-icon-rounded u-radius-20 u-icon-3"><img src="https://assets.nicepagecdn.com/9de13a7a/4924205/images/2931363-bd285455.png" alt=""></span>
-                <div class="u-align-left u-container-style u-grey-10 u-group u-radius-20 u-shape-round u-group-3">
-                  <div class="u-container-layout u-container-layout-6">
-                    <h5 class="u-custom-font u-font-montserrat u-text u-text-7">Apliances</h5>
-                    <p class="u-text u-text-8"> Choose quality kitchen tools that suit your needs, are easy to use, safe and easy to clean.</p>
-                  </div>
-                </div>
+            <div class="box">
+              <div class="cart">
+                <h2 class="title"> Apliances</h2>
+                <p class=""> Choosing home appliances is an important decision to make to ensure that you have the tools you need to work from home. Here are some expert tips on choosing home appliances: <br>
+                  <br>1. Choose high-quality home appliances: Choosing high-quality home appliances will help you use them for a long time and save on repair or replacement costs. Choose home appliances that are made from high-quality and durable materials. <br>
+                  <br>2. Choose the right home appliances for your needs: Choose the right home appliances for your needs. If you often cook, you should choose household appliances such as an oven, gas stove or electric stove. If you often work with computers, you should choose a computer that suits your needs. <br>
+                  <br>3. Choose energy-saving household appliances: Choosing energy-saving home appliances will help you save electricity costs and protect the environment. Choose appliances with energy-saving labels or with energy-saving features. <br>
+                  <br>4. Choose appliances that are easy to use: Choose appliances that are easy to use and easy to clean. Choose household appliances that are simple in design and easy to use and clean.
+                </p>
               </div>
             </div>
-            <div class="u-align-left u-border-2 u-border-grey-75 u-container-style u-image u-list-item u-repeater-item u-shading u-image-4" data-image-width="960" data-image-height="576">
-              <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-7"><span class="u-custom-color-1 u-file-icon u-icon u-icon-rounded u-radius-20 u-icon-4"><img src="https://assets.nicepagecdn.com/9de13a7a/4924205/images/3613691-14b8f4fb.png" alt=""></span>
-                <div class="u-align-left u-container-style u-grey-10 u-group u-radius-20 u-shape-round u-group-4">
-                  <div class="u-container-layout u-container-layout-8">
-                    <h5 class="u-custom-font u-font-montserrat u-text u-text-9"> Food Storage</h5>
-                    <p class="u-text u-text-10"> To preserve food well and ensure health safety, it is necessary to store it at the right temperature, use quality packaging, arrange it properly, clean the refrigerator regularly, and check the food regularly.</p>
-                  </div>
-                </div>
+            <div class="box">
+              <div class="cart">
+                <h2 class="title"> Food Storage</h2>
+                <p class=""> Choosing food storage products is an important part of food preservation and food safety. Here are some expert tips on choosing food storage products: <br>
+                  <br>1. Choose high-quality food storage products: Choosing high-quality food storage products will help you preserve food for a long time and keep food fresher. Choose food storage products that are made from high-quality and durable materials. <br>
+                  <br>2. Choose the right food storage products for your needs: Choose the right food storage products for your needs. If you regularly store food in the refrigerator, you should choose food storage products that can withstand low temperatures. If you often carry food away, you should choose food storage products that are spill-resistant and easy to carry. <br>
+                  <br>3. Choose safe food storage products: Choosing safe food storage products will help you ensure food safety and avoid health related problems. Choose food storage products that are resistant to bacteria and other harmful agents. <br>
+                  <br>4. Choose food storage products that are easy to use: Choose food storage products that are easy to use and easy to clean. Choose food storage products that are simple in design and easy to use and clean. <br>
+                  <br>5. Choose food storage products that fit your budget: Choose food storage products that fit your budget. High-quality food storage products often cost more, so you should think carefully before buying.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-   
-</body></html>
-<?php
-include('footer.php');
-?>
+  <?php
+  include('footer.php');
+  ?>
+  
