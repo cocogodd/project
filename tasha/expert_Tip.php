b4641b2a64696f1a87a406a62f1cecc2289af90a
phamtuan25 <tuan.pd.2233@apteachlearning>
2023-05-17 22:34:29 +0700

    fix css text

--- tasha/expert_Tip.php
+++ tasha/expert_Tip.php
@@ -30,7 +30,7 @@ include('header.php');
       text-shadow: 0px 2px 8px rgba(128,128,128,1);
     }
 
-    p{
+    .textdetail{
       font-family: "Times New Roman Times", sans-serif;
       font-size:20px;
       text-shadow: 0px 2px 5px white;
@@ -44,7 +44,7 @@ include('header.php');
             <div class="box">
               <div class="cart">
                 <h2 class="title">Cookware</h2>
-                <p class=""> Choosing cookware is an important decision to make sure that you can cook deliciously and safely. Here are some expert tips on choosing cookware: <br>
+                <p class="textdetail"> Choosing cookware is an important decision to make sure that you can cook deliciously and safely. Here are some expert tips on choosing cookware: <br>
                   <br>1. Choose high-quality cookware: High-quality cookware will help you cook better and prolong their life. Choose cookware made from high-quality materials such as stainless steel, copper, aluminum or cast iron. <br>
                   <br>2. Choose the right cookware for your needs: Choose the right cookware for your needs. If you often cook for a large family, you should choose a larger pot. If you often cook for one or two people, you can choose a smaller pot. <br>
                   <br>3. Choose cookware that is easy to use: Choose cookware that is easy to use and easy to clean. Choose cookware with comfortable and non-slip handles to avoid accidents while cooking. <br>
@@ -56,7 +56,7 @@ include('header.php');
             <div class="box">
               <div class="cart">
                 <h2 class="title"> Refrigeration</h2>
-                <p class=""> Choosing refrigeration equipment is an important decision to ensure that your product is in fact well managed and safe. Here are some expert tips on choosing refrigeration equipment: <br>
+                <p class="textdetail"> Choosing refrigeration equipment is an important decision to ensure that your product is in fact well managed and safe. Here are some expert tips on choosing refrigeration equipment: <br>
                   <br>1. Choose high-quality refrigeration equipment: Choosing high-quality refrigeration equipment will help you better manage your products and extend their life. Choose refrigeration equipment made from high-quality and durable materials. <br>
                   <br>2. Choose the right refrigerating appliance for your needs: Choose the right refrigerating appliance for your needs. If you often buy food in bulk, you should choose a larger refrigerator. If you live alone or with only a few people, you can choose a smaller refrigerator. <br>
                   <br>3. Choose energy-saving refrigeration equipment: Choosing energy-saving refrigeration equipment will help you save energy and protect the environment. Choose refrigeration equipment with an energy-efficient label or with an energy-saving feature. <br>
@@ -68,7 +68,7 @@ include('header.php');
             <div class="box">
               <div class="cart">
                 <h2 class="title"> Apliances</h2>
-                <p class=""> Choosing home appliances is an important decision to make to ensure that you have the tools you need to work from home. Here are some expert tips on choosing home appliances: <br>
+                <p class="textdetail"> Choosing home appliances is an important decision to make to ensure that you have the tools you need to work from home. Here are some expert tips on choosing home appliances: <br>
                   <br>1. Choose high-quality home appliances: Choosing high-quality home appliances will help you use them for a long time and save on repair or replacement costs. Choose home appliances that are made from high-quality and durable materials. <br>
                   <br>2. Choose the right home appliances for your needs: Choose the right home appliances for your needs. If you often cook, you should choose household appliances such as an oven, gas stove or electric stove. If you often work with computers, you should choose a computer that suits your needs. <br>
                   <br>3. Choose energy-saving household appliances: Choosing energy-saving home appliances will help you save electricity costs and protect the environment. Choose appliances with energy-saving labels or with energy-saving features. <br>
@@ -79,7 +79,7 @@ include('header.php');
             <div class="box">
               <div class="cart">
                 <h2 class="title"> Food Storage</h2>
-                <p class=""> Choosing food storage products is an important part of food preservation and food safety. Here are some expert tips on choosing food storage products: <br>
+                <p class="textdetail"> Choosing food storage products is an important part of food preservation and food safety. Here are some expert tips on choosing food storage products: <br>
                   <br>1. Choose high-quality food storage products: Choosing high-quality food storage products will help you preserve food for a long time and keep food fresher. Choose food storage products that are made from high-quality and durable materials. <br>
                   <br>2. Choose the right food storage products for your needs: Choose the right food storage products for your needs. If you regularly store food in the refrigerator, you should choose food storage products that can withstand low temperatures. If you often carry food away, you should choose food storage products that are spill-resistant and easy to carry. <br>
                   <br>3. Choose safe food storage products: Choosing safe food storage products will help you ensure food safety and avoid health related problems. Choose food storage products that are resistant to bacteria and other harmful agents. <br>
